21ed668643ff2bb5cb093b1ca3bf57b9050d2d5d
Konstantin <kaplan.sk8@mail.ru>
2023-06-14 15:38:22 +0300

    next

--- src/routes/paths.ts
+++ src/routes/paths.ts
@@ -1,7 +1,7 @@
 const SITE_NAME = "sbercloud-task";
 
 const MAIN_ROUTE = SITE_NAME + "/";
-const CREATE_ROUTE = SITE_NAME + "/create";
-const ERROR_ROUTE = SITE_NAME + "/error";
+const CREATE_ROUTE = "create";
+const ERROR_ROUTE = "error";
 
 export { MAIN_ROUTE, CREATE_ROUTE, ERROR_ROUTE };
