6dcf9e15f5af9722d9330f5282d2b34adef031aa
Konstantin <kaplan.sk8@mail.ru>
2023-06-14 15:29:10 +0300

    fix route

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,19 +1,13 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { Main } from "../pages/main/Main";
 import { Create } from "../pages/create/Create";
-import { Error } from "../pages/Error";
-import { MAIN_ROUTE, CREATE_ROUTE, ERROR_ROUTE } from "./paths";
+import { MAIN_ROUTE, CREATE_ROUTE } from "./paths";
 
 export function AppRouter() {
 	return (
 		<Routes>
 			<Route path={MAIN_ROUTE} element={<Main />} />
 			<Route path={CREATE_ROUTE} element={<Create />} />
-			<Route path={ERROR_ROUTE} element={<Error />} />
-			<Route
-				path={"*"}
-				element={<Navigate to={ERROR_ROUTE} replace={true} />}
-			/>
 		</Routes>
 	);
 }
