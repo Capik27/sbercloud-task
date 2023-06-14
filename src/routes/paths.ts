ba746c6da5e13279153dbcaf04e54543835995b2
Konstantin <kaplan.sk8@mail.ru>
2023-06-14 15:08:12 +0300

    fix routes

--- src/routes/paths.ts
+++ src/routes/paths.ts
@@ -1,5 +1,7 @@
-const MAIN_ROUTE = "/";
-const CREATE_ROUTE = "/create";
-const ERROR_ROUTE = "error";
+const SITE_NAME = "sbercloud-task";
+
+const MAIN_ROUTE = SITE_NAME + "/";
+const CREATE_ROUTE = SITE_NAME + "/create";
+const ERROR_ROUTE = SITE_NAME + "/error";
 
 export { MAIN_ROUTE, CREATE_ROUTE, ERROR_ROUTE };
